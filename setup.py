12b04a5d8dd6f5fdc88a7aa941622ba377c7b654
EmmanuelJeanBriand <36017203+EmmanuelJeanBriand@users.noreply.github.com>
2021-01-31 22:57:25 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+__project__ = "bbMathNQuizz"
+__version__ = "0.0.1"
+__description__ = "Tools for formatting text with math for Blackboad Learn."
+__packages__ = ["blathjax"]
+__author__ = "Emmnuel Briand"
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+)
